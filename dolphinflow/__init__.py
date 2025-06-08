af30332c4bb025b53ee7cc6e69690f601097b245
Eric Hartford <ehartford@gmail.com>
2025-06-08 02:14:39 -0400

    fix

--- dolphinflow/__init__.py
+++ dolphinflow/__init__.py
@@ -1,13 +1,13 @@
 # dolphinfow/__init__.py
 
 # The standard, dependency-free version is always available.
-from .dolphinfow import DolphinFlow
+from .dolphinflow import DolphinFlow
 
 # The 8-bit version is an optional feature. We try to import it,
 # but if the dependency is not met, we don't expose it and the
 # package remains usable.
 try:
-    from .dolphinfow_8bit import DolphinFlow8bit
+    from .dolphinflow_8bit import DolphinFlow8bit
 except ImportError:
     # If bitsandbytes is not installed, DolphinFlow8bit will not be available.
     # We pass silently, as this is expected behavior for an optional dependency.
